90365f4bf67c8de34014cf95a3f7eb942e91d5bc
dengxingquan <dengxingquan@xgd.com>
2025-04-28 13:57:39 +0800

    Initial commit

---
+++ src/main/java/com/xqcoder/toolbox/ToolboxApplication.java
@@ -0,0 +1,18 @@
+package com.xqcoder.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author dengxingquan
+ * @Description 启动类
+ * @Date 2025/4/28 13:56
+ */
+@SpringBootApplication
+public class ToolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolboxApplication.class, args);
+	}
+
+}
